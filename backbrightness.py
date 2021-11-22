97055dcc47593fbb02ea60b2fa4d78c800956b31
Zoltán <islandofcalmness@gmail.com>
2021-11-22 08:46:55 +0100

    modernize the code a bit

--- backbrightness.py
+++ backbrightness.py
@@ -5,31 +5,34 @@
 
 import os
 import time
+from abc import ABC, abstractmethod
 from pathlib import Path
 from functools import lru_cache
+from typing import Optional
 
 
-class BrightnessSetter():
-    def __init__(self, output_names):
+class BrightnessSetter(ABC):
+    def __init__(self, output_names: list[str]):
         self.crtcs = {}
         self.crtcs = {}
         for name in output_names:
             crtc = {
-                    'id': None,
-                    'original_gamma': None,
-                    }
+                'id': None,
+                'original_gamma': None,
+            }
             self.crtcs[name] = crtc
 
     @lru_cache(maxsize=10)
-    def generate_gamma_table(self, crtc_name, brightness=1.0):
+    def generate_gamma_table(self, crtc_name: str, brightness: float = 1.0):
         table = self.crtcs[crtc_name]['original_gamma']
         if brightness is None:
             return table
         return tuple(
-                list(
-                    int(i * brightness) for i in channel
-                    ) for channel in table
-                )
+            list(int(i * brightness) for i in channel) for channel in table)
+
+    @abstractmethod
+    def set_brightness(self, brightness: Optional[float]):
+        pass
 
     def reset(self):
         self.set_brightness(None)
@@ -37,7 +40,10 @@ class BrightnessSetter():
 
 # this has the happy consequence of resetting gamma
 class XRandRBrightnessSetter(BrightnessSetter):
-    def __init__(self, output_names=[], display_name=None, screen_num=None):
+    def __init__(self,
+                 output_names: list[str] = [],
+                 display_name: Optional[str] = None,
+                 screen_num: Optional[int] = None):
         super().__init__(output_names)
         self.connection = None
         self.display_name = display_name
@@ -52,9 +58,8 @@ class XRandRBrightnessSetter(BrightnessSetter):
         for crtc_id in crtc_ids:
             crtc_info = randr.GetCrtcInfo(crtc_id, int(time.time())).reply()
             for output in crtc_info.outputs:
-                output_info = randr.GetOutputInfo(
-                        output, int(time.time())
-                        ).reply()
+                output_info = randr.GetOutputInfo(output,
+                                                  int(time.time())).reply()
                 name = bytes(output_info.name).decode('ascii')
                 print('met crtc {}'.format(name))
                 crtc = self.crtcs.get(name)
@@ -63,7 +68,8 @@ class XRandRBrightnessSetter(BrightnessSetter):
                     if crtc['original_gamma'] is None:
                         reply = randr.GetCrtcGamma(crtc_id).reply()
                         crtc['original_gamma'] = [
-                                reply.red, reply.green, reply.blue]
+                            reply.red, reply.green, reply.blue
+                        ]
 
     def connect(self):
         import xcffib
@@ -75,10 +81,8 @@ class XRandRBrightnessSetter(BrightnessSetter):
             return value
 
         if not self.connection:
-            disp_name = default_if_none(
-                    self.display_name,
-                    os.environ.get('DISPLAY')
-                    )
+            disp_name = default_if_none(self.display_name,
+                                        os.environ.get('DISPLAY'))
             self.connection = con = xcffib.connect(disp_name)
             screen_num = default_if_none(self.screen_num, con.pref_screen)
             self.screen = con.get_setup().roots[screen_num]
@@ -87,25 +91,22 @@ class XRandRBrightnessSetter(BrightnessSetter):
 
         return self.randr
 
-    def set_brightness(self, brightness):
+    def set_brightness(self, brightness: Optional[float]):
         randr = self.connect()
+        assert self.connection
         for name, crtc in self.crtcs.items():
             if crtc['id'] is None:
                 continue
 
             adjusted = self.generate_gamma_table(name, brightness)
-            randr.SetCrtcGamma(
-                    crtc['id'],
-                    len(adjusted[0]),
-                    adjusted[0],
-                    adjusted[1],
-                    adjusted[2])
+            randr.SetCrtcGamma(crtc['id'], len(adjusted[0]), adjusted[0],
+                               adjusted[1], adjusted[2])
         self.connection.flush()
 
 
 # this is pretty damn resource intensive
 class GnomeBrightnessSetter(BrightnessSetter):
-    def __init__(self, output_names=[]):
+    def __init__(self, output_names: list[str] = []):
         super().__init__(output_names)
         from pydbus import SessionBus
 
@@ -134,24 +135,21 @@ class GnomeBrightnessSetter(BrightnessSetter):
                 crtc['id'] = crtc_id
                 if crtc['original_gamma'] is None:
                     gamma = self.display_conf.GetCrtcGamma(
-                            self.serial, crtc_id)
+                        self.serial, crtc_id)
                     crtc['original_gamma'] = gamma
 
-    def set_brightness(self, brightness):
+    def set_brightness(self, brightness: Optional[float]):
         self.init_configuration()
         for name, crtc in self.crtcs.items():
             if crtc['id'] is None:
                 continue
 
             gamma = self.generate_gamma_table(name, brightness)
-            self.display_conf.SetCrtcGamma(
-                    self.serial,
-                    crtc['id'],
-                    gamma[0], gamma[1], gamma[2]
-                    )
+            self.display_conf.SetCrtcGamma(self.serial, crtc['id'], gamma[0],
+                                           gamma[1], gamma[2])
 
 
-def translate_backlight(setter, backlight_path, sleep_time):
+def translate_backlight(setter, backlight_path: Path, sleep_time: float):
     max_path = backlight_path / 'max_brightness'
     actual_path = backlight_path / 'actual_brightness'
 
@@ -176,44 +174,38 @@ def main():
 
     default_method = 'xrandr'
     methods = {
-            'xrandr': XRandRBrightnessSetter,
-            'gnome': GnomeBrightnessSetter,
-        }
+        'xrandr': XRandRBrightnessSetter,
+        'gnome': GnomeBrightnessSetter,
+    }
 
     parser = argparse.ArgumentParser(
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter
-            )
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
-            '-s', '--sleep-time',
-            type=float,
-            default=1.0,
-            help='Time between two brightness updates. Lower is more '
-            'responsive, but higher CPU usage.'
-            )
+        '-s',
+        '--sleep-time',
+        type=float,
+        default=1.0,
+        help='Time between two brightness updates. Lower is more '
+        'responsive, but higher CPU usage.')
+    parser.add_argument('backlight_path',
+                        type=Path,
+                        help='Path for the intel acpi backlight. For example '
+                        '"/sys/class/backlight/intel_backlight"')
     parser.add_argument(
-            'backlight_path',
-            type=str,
-            help='Path for the intel acpi backlight. For example '
-            '"/sys/class/backlight/intel_backlight"'
-            )
-    parser.add_argument(
-            'outputs',
-            type=str,
-            nargs='+',
-            help='outputs whose brightness to adjust with randr. Check '
-            '"xrandr -q" for values. Normally it should be eDP1 or eDP-1'
-            )
-    parser.add_argument(
-            '--setter-method',
-            default=default_method,
-            choices=methods.keys(),
-            help='Which setter method to use.'
-            )
+        'outputs',
+        type=str,
+        nargs='+',
+        help='outputs whose brightness to adjust with randr. Check '
+        '"xrandr -q" for values. Normally it should be eDP1 or eDP-1')
+    parser.add_argument('--setter-method',
+                        default=default_method,
+                        choices=methods.keys(),
+                        help='Which setter method to use.')
 
     args = parser.parse_args()
 
     setter = methods[args.setter_method](args.outputs)
-    translate_backlight(setter, Path(args.backlight_path), args.sleep_time)
+    translate_backlight(setter, args.backlight_path, args.sleep_time)
 
     return 0
 
